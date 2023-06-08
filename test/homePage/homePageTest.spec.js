bc5665aa1207d526a3d893fb843731b04ffda930
Dorottya Pecsi <dorottya.pecsi@gmail.com>
2023-06-08 10:32:50 +0200

    added webdriverIO

--- test/homePage/homePageTest.spec.js
+++ test/homePage/homePageTest.spec.js
@@ -1,1 +1,8 @@
-// TODO
+describe('Open HomePage', () => {
+  it('should open home page and validate the title', async () => {
+    await browser.url('https://demoqa.com/');
+    const pageTitle = await browser.getTitle();
+
+    expect(pageTitle).toEqual('DEMOQA');
+  });
+});
